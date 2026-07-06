acef0729c82aaa33221967818fcc1405c06df37d
Ivo Kostovski <ivo.kostovski@gmail.com>
2026-07-06 20:24:30 +0200

    feat: implement useLocalStorage hook and update FavoritesProvider to use it

--- class_07_context/pantry-pal/src/context/FavoritesContext.tsx
+++ class_07_context/pantry-pal/src/context/FavoritesContext.tsx
@@ -1,15 +1,19 @@
-import { useState, type ReactNode } from 'react';
+import { type ReactNode } from 'react';
 import { FavoritesContext } from './favorites-context';
+import { useLocalStorage } from '../hooks/useLocalStorage';
 
 export function FavoritesProvider({ children }: { children: ReactNode }) {
-	const [favoritesIds, setFavoritesIds] = useState<string[]>([]);
+	const [favoritesIds, setFavoritesIds] = useLocalStorage<string[]>(
+		'PANTRY_PAL-FAVORITES',
+		[],
+	);
 
 	const toggleFavorite = (id: string) => {
 		setFavoritesIds(currentIds => {
 			const isInList = currentIds.includes(id);
 
 			if (isInList) {
-				currentIds = currentIds.filter(id => id !== id);
+				currentIds = currentIds.filter(_id => _id !== id);
 			} else {
 				// we update the array values WITHOUT changing the reference
 				// currentIds.push(id);
